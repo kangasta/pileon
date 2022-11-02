2e259e551393526fa42fd95005c7e6cc19fd1b6a
Toni Kangas <toni.ar.kangas@gmail.com>
2022-11-02 02:34:59 +0200

    Add initial pileon solitaire dealing logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Deck from "./views/Deck.svelte"
+  import Pileon from "./views/Pileon.svelte"
   import Footer from "./components/Footer.svelte";
   import Menu from "./components/Menu/Menu.svelte";
+
+  const View = location.hash.toLowerCase() === "#pileon" ? Pileon : Deck
 </script>
 
 <Menu />
-<Deck />
+<View />
 <Footer />
