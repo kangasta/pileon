7c3006bbec63b049d8bcccf7b120c5e93071d6a6
Toni Kangas <toni.ar.kangas@gmail.com>
2023-01-24 22:49:07 +0200

    Trigger undo also with `Ctrl+z`

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,23 @@
   import Pileon from "./views/Pileon.svelte";
   import Footer from "./components/Footer.svelte";
   import Menu from "./components/Menu/Menu.svelte";
-  import { settings } from "./stores";
+  import { actions, settings } from "./stores";
 
   $: View = $settings.game === "pileon" ? Pileon : Deck;
+
+  // Handle global keyboard shortcuts, e.g. undo with ctrl+z.
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (
+      e.key === "z" &&
+      (e.ctrlKey || e.metaKey) &&
+      $actions.undo !== undefined
+    ) {
+      $actions.undo(e);
+    }
+  };
 </script>
 
 <Menu />
 <svelte:component this={View} />
+<svelte:body on:keydown={onKeyDown} />
 <Footer />
