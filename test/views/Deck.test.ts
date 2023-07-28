3dcdaa7bf152309c51b0181ff105221dcf9de4c2
Toni Kangas <toni.ar.kangas@gmail.com>
2023-07-29 01:33:59 +0300

    Add test for clicking through the deck

--- test/views/Deck.test.ts
+++ test/views/Deck.test.ts
@@ -1,7 +1,21 @@
 import { render, screen } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
 import Deck from "../../src/views/Deck.svelte";
 
 it("renders the deck", async () => {
   render(Deck);
+  await screen.findByLabelText("Draw a card");
+});
+
+it("cycles through the deck", async () => {
+  render(Deck);
+  const deck = await screen.findByLabelText("Draw a card");
+
+  for (let i = 0; i < 53; i++) {
+    await userEvent.click(deck);
+  }
+
+  await screen.findByLabelText("Shuffle the deck");
+  await userEvent.click(deck);
   await screen.findByLabelText("Draw a card");
 });
