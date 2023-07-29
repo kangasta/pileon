41241784e2db1613a5d74b0cecdb6a8fbac6bfd5
Toni Kangas <toni.ar.kangas@gmail.com>
2023-07-30 02:48:45 +0300

    Add tests for shuffle button

---
+++ test/App.test.ts
@@ -0,0 +1,47 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
+import App from "../src/App.svelte";
+import {
+  checkDeckHasAceOfSpades,
+  createFocusTrapMock,
+  waitShuffleAnimation,
+} from "./utils";
+import * as focusTrap from "focus-trap";
+
+vi.spyOn(focusTrap, "createFocusTrap").mockImplementation(createFocusTrapMock);
+
+it("displays shuffle and undo buttons", async () => {
+  const { container } = render(App);
+
+  const cards = container.getElementsByClassName("card");
+  expect(cards).toHaveLength(54); // 52 Cards + 2 Empty cards
+
+  await screen.findByLabelText("Shuffle");
+  await screen.findByLabelText("Undo");
+});
+
+it("allows shuffling the deck", async () => {
+  render(App);
+
+  // Change game to deck
+  await userEvent.click(await screen.findByLabelText("Settings"));
+  await userEvent.click(await screen.findByRole("button", { name: "deck" }));
+  await userEvent.click(await screen.findByLabelText("Close"));
+
+  const deck = await waitShuffleAnimation();
+
+  for (let i = 0; i < 10; i++) {
+    await userEvent.click(deck);
+  }
+
+  await screen.findByLabelText("Draw a card");
+  expect(screen.queryByText("Shuffle the deck")).not.toBeInTheDocument();
+
+  const shuffleButton = await screen.findByLabelText("Shuffle");
+  await userEvent.click(shuffleButton);
+  await waitShuffleAnimation();
+
+  await checkDeckHasAceOfSpades(deck);
+  await screen.findByLabelText("Shuffle the deck");
+}, 10e3);
--- test/views/Deck.test.ts
+++ test/views/Deck.test.ts
@@ -1,7 +1,8 @@
 import "@testing-library/jest-dom";
-import { render, screen, waitFor } from "@testing-library/svelte";
+import { render, screen } from "@testing-library/svelte";
 import userEvent from "@testing-library/user-event";
 import Deck from "../../src/views/Deck.svelte";
+import { checkDeckHasAceOfSpades, waitShuffleAnimation } from "../utils";
 
 it("renders the deck", async () => {
   render(Deck);
@@ -10,24 +11,9 @@ it("renders the deck", async () => {
 
 it("cycles through the deck", async () => {
   render(Deck);
-  const deck = await screen.findByLabelText("Draw a card");
+  const deck = await waitShuffleAnimation();
 
-  // Wait for shuffle animation to finish
-  await waitFor(
-    () => expect(deck).not.toHaveAttribute("aria-disabled", "true"),
-    { timeout: 1500 },
-  );
-
-  let aceOfSpadesN = 0;
-  for (let i = 0; i < 53; i++) {
-    await userEvent.click(deck);
-
-    if (screen.queryByText("ace of spades")) {
-      aceOfSpadesN++;
-    }
-  }
-
-  expect(aceOfSpadesN).toEqual(1);
+  await checkDeckHasAceOfSpades(deck);
   await screen.findByLabelText("Shuffle the deck");
   await userEvent.click(deck);
   await screen.findByLabelText("Draw a card");
