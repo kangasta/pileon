ac952dd63b912528eb6eccd54a360af96b269c8e
Toni Kangas <kangasta@users.noreply.github.com>
2023-10-20 01:56:09 +0300

    Add logic for detecting multiple infinite loops (#95)

--- test/utils/pileon.test.ts
+++ test/utils/pileon.test.ts
@@ -12,12 +12,21 @@ describe("isDeadEnd", () => {
     ];
     expect(isDeadEnd(piles)).toEqual(false);
   });
-  it("recognizes dead-end", () => {
+  it("recognizes dead end", () => {
     const piles = [new Cards("8♠ 5♦ J♣ 4♥"), new Cards("3♠ Q♦ J♠ 4♠")];
     expect(isDeadEnd(piles)).toEqual("dead-end");
   });
-  it("recognizes infinite-loop", () => {
+  it("recognizes infinite loop", () => {
     const piles = [new Cards("K♥ A♣ 7♦"), new Cards("8♠ 5♥ 7♥ 7♠")];
     expect(isDeadEnd(piles)).toEqual("infinite-loop");
+  });
+  it("recognizes multiple infinite loops", () => {
+    const piles = [
+      new Cards("J♠ Q♠ 7♥ 7♦"),
+      new Cards("K♥ 4♣ 7♠"),
+      new Cards("J♥ J♦ Q♦ Q♣"),
+      new Cards("4♦ K♠ Q♥"),
+    ];
+    expect(isDeadEnd(piles)).toEqual("multi-infinite-loop");
   });
 });
