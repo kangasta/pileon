0e2b52d8470fef46fc439a09b902cc071cfb6045
Toni Kangas <kangasta@users.noreply.github.com>
2024-06-04 00:34:20 +0300

    Use side-drawer for settings menu (#125)

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -7,10 +7,12 @@
   import IconButton from "./Menu/IconButton.svelte";
 
   export let title: string;
+  export let position: "left" | "center" = "center";
 
   const id = `modal-${randomString()}`;
   const titleId = `${id}-title`;
 
+  let open = false;
   onMount(() => {
     const trap = focusTrap.createFocusTrap(`#${id}`, {
       clickOutsideDeactivates: true,
@@ -20,6 +22,10 @@
     const bodyOverflow = document.body.style.overflow;
     document.body.style.overflow = "hidden";
 
+    setTimeout(() => {
+      open = true;
+    }, 0);
+
     return () => {
       trap.deactivate();
       document.body.style.overflow = bodyOverflow;
@@ -28,12 +34,19 @@
 
   const dispatch = createEventDispatcher();
   const onClose = () => {
-    dispatch("close");
+    open = false;
+    setTimeout(() => dispatch("close"), 125);
   };
 </script>
 
-<div class="backdrop" />
-<div class="modal" role="dialog" aria-labelledby={titleId} {id}>
+<div class="backdrop" class:open />
+<div
+  class="modal {position}"
+  class:open
+  role="dialog"
+  aria-labelledby={titleId}
+  {id}
+>
   <div class="header">
     <h2 id={titleId}>{title}</h2>
     <IconButton icon="Close" label="Close" onClick={onClose} />
@@ -45,7 +58,6 @@
 
 <style lang="sass">
   .backdrop
-    animation: blur-background 125ms forwards
     position: fixed
     top: 0
     left: 0
@@ -55,47 +67,70 @@
 
     z-index: 4
 
-    @keyframes blur-background
-      from
-        backdrop-filter: none
+    backdrop-filter: none
+    transition: backdrop-filter 125ms
 
-      to
-        backdrop-filter: blur(4px)
+    &.open
+      backdrop-filter: blur(4px)
 
   .modal
-    animation: open-modal 125ms forwards
     display: flex
     flex-direction: column
 
     position: fixed
-    top: 50%
-    left: 50%
+
+    &.center
+      top: 50%
+      left: 50%
+
+      max-height: 90%
+      width: 500px
+      max-width: 100%
+
+      padding: 0 1em 2em
+
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.75)
+
+      opacity: 0
+      transform: translate(-50%, -25%) scale(0.25)
+      transition: opacity 125ms, transform 125ms
+
+      &.open
+        opacity: 1
+        transform: translate(-50%, -50%) scale(1)
+
+      @media (min-width: 501px)
+        border-radius: 0.25em
+
+    &.left
+      top: -2vh
+      left: 0
+
+      height: 104vh
+      max-width: 95%
+
+      .content
+        flex: 1
+
+      padding: 2vh 1em
+
+      box-shadow: 0.5rem 0 1rem rgba(0, 0, 0, 0.75)
+
+      opacity: 0
+      transform: translateX(-50%)
+      transition: opacity 125ms, transform 125ms
+
+      &.open
+        opacity: 1
+        transform: translateX(0)
 
     background: white
     z-index: 4
 
     font-size: 1rem
-    padding: 0 1em 2em
     overflow: hidden
 
     box-sizing: border-box
-    max-height: 90%
-    width: 500px
-    max-width: 100%
-
-    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.75)
-
-    @keyframes open-modal
-      from
-        opacity: 0
-        transform: translate(-50%, -25%) scale(0.25)
-
-      to
-        opacity: 1
-        transform: translate(-50%, -50%) scale(1)
-
-    @media (min-width: 501px)
-      border-radius: 0.25em
 
     .header
       display: flex
