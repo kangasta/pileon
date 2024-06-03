a88a65feddf8984d05775e35d3ea49bfb81ef66f
Toni Kangas <toni.ar.kangas@gmail.com>
2024-06-03 23:25:59 +0300

    Animate modal in both directions

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -11,6 +11,7 @@
   const id = `modal-${randomString()}`;
   const titleId = `${id}-title`;
 
+  let open = false;
   onMount(() => {
     const trap = focusTrap.createFocusTrap(`#${id}`, {
       clickOutsideDeactivates: true,
@@ -20,6 +21,10 @@
     const bodyOverflow = document.body.style.overflow;
     document.body.style.overflow = "hidden";
 
+    setTimeout(() => {
+      open = true;
+    }, 0);
+
     return () => {
       trap.deactivate();
       document.body.style.overflow = bodyOverflow;
@@ -28,12 +33,19 @@
 
   const dispatch = createEventDispatcher();
   const onClose = () => {
-    dispatch("close");
+    open = false;
+    setTimeout(() => dispatch("close"), 125);
   };
 </script>
 
-<div class="backdrop" />
-<div class="modal" role="dialog" aria-labelledby={titleId} {id}>
+<div class="backdrop" class:open />
+<div
+  class="modal {position}"
+  class:open
+  role="dialog"
+  aria-labelledby={titleId}
+  {id}
+>
   <div class="header">
     <h2 id={titleId}>{title}</h2>
     <IconButton icon="Close" label="Close" onClick={onClose} />
@@ -45,7 +57,6 @@
 
 <style lang="sass">
   .backdrop
-    animation: blur-background 125ms forwards
     position: fixed
     top: 0
     left: 0
@@ -55,21 +66,29 @@
 
     z-index: 4
 
-    @keyframes blur-background
-      from
-        backdrop-filter: none
+    backdrop-filter: none
+    transition: backdrop-filter 125ms
 
-      to
-        backdrop-filter: blur(4px)
+    &.open
+      backdrop-filter: blur(4px)
 
   .modal
-    animation: open-modal 125ms forwards
     display: flex
     flex-direction: column
 
     position: fixed
-    top: 50%
-    left: 50%
+
+    &.center
+      top: 50%
+      left: 50%
+
+      opacity: 0
+      transform: translate(-50%, -25%) scale(0.25)
+      transition: opacity 125ms, transform 125ms
+
+      &.open
+        opacity: 1
+        transform: translate(-50%, -50%) scale(1)
 
     background: white
     z-index: 4
@@ -84,15 +103,6 @@
     max-width: 100%
 
     box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.75)
-
-    @keyframes open-modal
-      from
-        opacity: 0
-        transform: translate(-50%, -25%) scale(0.25)
-
-      to
-        opacity: 1
-        transform: translate(-50%, -50%) scale(1)
 
     @media (min-width: 501px)
       border-radius: 0.25em
