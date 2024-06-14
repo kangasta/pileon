469044394cd921839ef25044c202a061a707fb92
Toni Kangas <toni.ar.kangas@gmail.com>
2024-06-15 01:41:09 +0300

    Allow re-opening completed and dead-end modals

---
+++ src/components/Menu/Icon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { icons, type IIconName } from "./icons";
+
+  export let icon: IIconName;
+</script>
+
+<svg viewBox="0 0 16 16">
+  <path
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1"
+    d={icons[icon].path}
+  />
+</svg>
--- src/components/Menu/IconButton.svelte
+++ src/components/Menu/IconButton.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
   import { randomString } from "../../utils/components";
 
-  type IIconName = "Close" | "Help" | "Menu" | "Shuffle" | "Undo";
-  const icons: { [key in IIconName]: { path: string } } = {
-    Close: {
-      path: "M 3 3 L 13 13 M 3 13 L 13 3",
-    },
-    Help: {
-      path: "M 8 12 L 8 12 M 8 10 L 8 9 C 8 7 10 8 10 6 A 2 2 0 1 0 6 6 M 8 1.5 A 6.5 6.5 0 0 1 8 14.5 A 6.5 6.5 0 0 1 8 1.5",
-    },
-    Menu: {
-      path: "M 2 4 L 14 4 M 2 8 L 14 8 M 2 12 L 14 12",
-    },
-    Shuffle: {
-      path: "M 5 2 L 2 5 L 5 8 M 2 5 L 12 5 M 11 14 L 14 11 L 11 8 M 14 11 L 4 11",
-    },
-    Undo: {
-      path: "M 5 3 L 2 6 L 5 9 M 2 6 L 10 6 A 3 3 0 1 1 10 13 L 8 13",
-    },
-  };
+  import Icon from "./Icon.svelte";
+  import type { IIconName } from "./icons";
 
   export let icon: IIconName;
   export let label: string;
@@ -37,16 +21,9 @@
       class:open
       on:click={onClick}
     >
-      <svg viewBox="0 0 16 16">
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1"
-          d={icons[icon].path}
-        />
-      </svg>
+      <div class="icon">
+        <Icon {icon} />
+      </div>
     </button>
     <div class="label" id={labelId}>{label}</div>
   </div>
@@ -65,7 +42,7 @@
     &:hover .content
       transform: scale(1.1) translateY(-0.15rem)
 
-    &:active svg
+    &:active .icon
       transform: translateY(0.15rem)
 
   div.content
@@ -89,7 +66,7 @@
       outline: 3px solid black
       border-radius: 0.5em
 
-    svg
+    :global(svg)
       height: 2rem
 
   div.label
